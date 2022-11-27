dd4986fe993f4a6d3b8aa06d2d05572f3bd14dc2
john Caropino <jccaropino7786@live.com>
2022-11-27 15:06:34 -0800

    commit tree 58b38fff0dc6d9a37956657b56ca1e2ba9c3f3ac

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+// function receivesAFunction(callback) {
+//     callback();
+// }
+  
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function returnsANamedFunction(){
+    return returnsANamedFunction
+}
+
+function returnsAnAnonymousFunction(){
+    return () => ("")
+    }
+
+
+
